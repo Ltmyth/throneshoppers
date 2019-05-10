a82551ff1918c90dae6f74f2413ee82478352d19
ltmyth <latimmark@gmail.com>
2019-05-10 10:08:13 +0300

    Throne Shoppers

---
+++ templates/index.php
@@ -0,0 +1,353 @@
+		<?php include('../includes/head.html'); ?>
+		<div id="first_div" class="container">
+			<div style="padding-top:5%;" class="row tp">
+				<div class="col-md ">
+					<h4>
+						<table style="width:40%;margin-left:30%;" class=" center">
+							<tr class="space">
+								<td class="btm"><b>Home</b></td>
+								<td>How it works</td>
+								<td>Shop</td>
+								<td class="btm">Ship</td>
+							</tr>
+							<tr style="border-top:1px solid;">
+								<td></td>
+								<td>Shippping Calculator</td>
+								<td>Export</td>
+								<td>Import</td>
+							</tr>
+						</table>
+					</h4>
+				</div>
+				<div style="float:right;margin-right:5%;padding-bottom:5%;" class="col-sm-4">
+					<a href="#">
+						<button id="login_btn" class='blue btn btn-lg '>
+							Login Or Register
+						</button>
+					</a>
+				</div>
+			</div>
+		<div class="container tp">
+			<div id="description" class=" row">
+				<div class="left col-md ">
+					<h1>
+						<b style="font-size:60px;font-weight:200px;">
+							Shop Online, <br>
+							We ship to you.
+						</b>
+					</h1>
+					<br>
+					<br>
+					<h4>
+						<p>Shop online with your credit card from any <br>
+						USA/UK stores and get your items delivered to you at any<br>
+						part of the country.</p>
+					</h4>
+					<br>
+					<button class='blue btn btn-lg'>
+						Go Shopping
+					</button>
+				</div>
+				<div class="col-sm-4 ">
+					<img style="width:200%;margin-right:5%;" src="../imgs/plane.png">
+				</div>				
+			</div>
+		</div>
+
+			<br>
+			<div style="text-align:center;margin-left:5%;border-radius:20px;background:white;width:90%;" class="row">
+				<div class="left col-sm-4">
+					<h3 style="background:url(../imgs/getaddress.png);background-size:100% 100%;color:white;">
+						&nbsp;&nbsp;
+						<b >Get US/UK address</b></h3>
+					<br>
+					<h5>
+						Get your US/UK address today, start shopping<br>
+						wherever you are and get your items delivered<br>
+						to your location.
+					</h5>
+				</div>
+				<div class="col-sm-4">
+					<img src="../imgs/map.png">
+				</div>
+				<div style="text-align:center;" class="col-sm-4">
+					<br>
+					<big>Login</big>
+					<hr style="width:40%;border:1px solid;">
+					<big>Register</big>
+				</div>
+			</div>
+				
+			<div style="margin-left:10%;padding-top:10%;padding-bottom:5%;" class="row">
+				<h2 id="howitworks" style="background:url(../imgs/how.png);background-size:100% 100%;color:white;width:25%;">
+					&nbsp;&nbsp;<b>How it works</b>
+					&nbsp;
+				</h2>
+				
+				<div class="col-sm-4" style="border-left:1px solid;border-color:#ffc048;">
+					<br>
+					<span style="border:1px solid;background:#ffc048;" class='left dot'>
+						<b>1</b>
+					</span>
+					<br>
+					<h4>
+						<b>
+							Register for free<br>
+							or Login
+						</b>
+					</h4>
+					<h5> 
+						<p>
+							Register for free and get a free US/UK<br>
+							address where your purchased items<br>
+							from US/UK stores will be delivered<br>
+							before being shipped to your country.<br>
+							<br>
+							Lorem ipsum dolor sit amet, adicing<br>
+							elit, sed do eiusmod tempor.
+						</p>
+					</h5>
+				</div>
+				<div class="col-sm-4" style="border-left:1px solid;border-color:#34e7e4;">
+					<br>
+					<span style="border:1px solid;" class='blue left dot'>
+						<b>2</b>
+					</span>
+					<br>
+					<h4>
+						<b>
+							Go shopping from the most<br>
+							popular stores anywhere.
+						</b>
+					</h4>
+					<h5>
+						<p>
+							Shop from online stores in the US/UK<br>
+							and get your items delivered to your<br>
+							Nigeria home address.<br>
+							<br>
+							Lorem ipsum dolor sit amet, adicing<br>
+							elit, sed do eiusmod tempor<br>
+							incididunt ut labore et dolore magna<br>
+							aliqua.
+						</p>
+					</h5>
+				</div>
+				<div class="col-sm-4" style="border-left:1px solid;border-color:red;">
+					<br>
+					<span style="border:1px solid;background:#ef5777;" class='left dot'>
+						<b>3</b>
+					</span>
+					<br>
+					<h4>
+						<b>
+							We package & wrap it up for that extra <br>
+							protection.
+						</b>
+					</h4>
+					<h5>
+						<p>
+							Lorem ipsum dolor sit amet,<br>
+							adicing	elit, sed do eiusmod<br> 
+							tempor incididunt ut labore et <br> 
+							dolore magna aliqua.
+						</p>
+					</h5>
+					<br>
+					<button class="right btn btn-lg btn-default">More Details</button>
+				</div>
+			</div>
+			<br>
+			<br><br><br>
+				<table style="width:90%;height:auto;margin-left:8%;
+					text-align:left;">
+					<tr>
+						<td>
+							<h3 style="background:url(../imgs/goshop.png);background-size:100% 100%;color:white;width:50%;">
+								&nbsp;&nbsp;<b>Go Shopping</b>
+							</h3>
+							<br>
+							<h4>
+								<p>
+									Go shopping with our trusted most<br>
+									partners using your preferable<br>
+									credit card.
+								</p>
+							</h4>
+						</td>
+						<td style="padding-right:70px;">Or</td>
+						<td>
+							<h3>
+								<b>
+									Let Us Shop For you
+								</b>
+							</h3>
+							<br>
+							<h4>
+								<p>
+									Lorem ipsum dolor sit amet,<br>
+									adicing	elit, sed do eiusmod<br> 
+									tempor incididunt ut labore et <br> 
+									dolore magna aliqua.
+								</p>
+							</h4>
+							<br>
+							<button class="btn btn-lg btn-default">
+								Shop for me
+							</button>
+						</td>
+					</tr>
+				</table>
+				<br>
+				<div style="padding:5% 0% 30% 0%;background:url(../imgs/logo_div.png);background-size:100% 100%;">
+					<img style="float:left;width:80%;" src="../imgs/logos.jpg">
+					<img style="float:right;margin-right:10%;" src="../imgs/right.png">
+				</div>
+				<br>
+				<div class="container">
+					<div style="margin-left:10%;" class='row'>
+						<h2 style="background:url(../imgs/getaddress.png);background-size:100% 100%;color:white;width:50%;">
+							&nbsp;&nbsp;
+							<b>Shipping your packages</b>
+						</h2>
+						<div class="col-sm-4">
+							<br>
+							<h4>
+								<p>
+									Ship your packages to and from the US/UK
+									<br>
+									with safety and arrival assurance.
+									<br><br>
+									Lorem ipsum dolor sit amet,adicing	elit,<br>
+									sed do eiusmod tempor incididunt ut <br>labore aliqua.
+
+								</p>
+							</h4>
+						</div>
+						<div class="col-sm-4">
+							<img src="../imgs/import.jpeg">
+							<h3>
+								<b>Import my package</b>
+							</h3>
+							<br>
+							<h4>
+								<p>
+									Lorem ipsum dolor sit amet,adicing<br>
+									elit, sed do eiusmod tempor dolore<br>
+									incididunt ut labore aliqua empor<br>
+									dolore incididuer.
+								</p>
+							</h4>
+							<br>
+							<img style="width:20%;" src="../imgs/ikon.png">
+						</div>
+
+						<div class="col-sm-4">
+							<img src="../imgs/export.jpeg">
+							<h3>
+								<b>Export my package</b>
+							</h3>
+							<br>
+							<h4>
+								<p>
+									Lorem ipsum dolor sit amet,adicing<br>
+									elit, sed do eiusmod tempor dolore<br>
+									incididunt ut labore aliqua empor<br>
+									dolore incididuer.
+								</p>
+							</h4>
+							<br>
+							<img style="width:20%;" src="../imgs/ikon.png">
+						</div>
+					</div>
+				</div>
+				<br>
+				<div style="padding:10% 0% 10% 0%;margin-left:5%;background:#f5f6fa;">
+					<table class='full'>
+						<tr>
+							<td style="background:#f5f6fa;">
+								<img src="../imgs/cards.jpg">
+							</td>
+							<td style="float:right;margin-right:5%;text-align:left;">
+								<h2 style="background:url(../imgs/how.png);background-size:100% 100%;color:white;">
+									&nbsp;
+									<b>Major credit cards accepted</b>
+									&nbsp;
+								</h2>
+								<h5>
+									<p>
+										Pay using Master card, Visa, Paypal and
+										<br>
+										AmericanExpress cards. Dont worry about
+										<br>
+										safety, we got you all covered.
+									</p>
+								</h5>
+							</td>
+						</tr>
+					</table>
+				</div>
+				<br>
+			</div>
+	</body>
+
+		<div class='container' style="text-align:center;background:url(../imgs/foot.png);background-size:100% 100%;color:white;width:100%;padding:5% 0px 0px 0px;">
+			<table class='left' style="margin-left:10%;">
+				<tr>
+					<td>
+						<img src="../imgs/logo.png">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						
+						<h5>
+							<p>
+								45678 Jwata Lane<br>
+								Lagos, Lorem 234 Nigeria<br>
+								<b>Phone:</b>
+								+234 456 345 5577<br>
+								<b>Email:</b>infothrone@throneshoppers.com
+							</p>
+						</h5>
+					</td>
+				</tr>
+			</table>
+			<div class='row right' style="margin-right:10%;width:35%;text-align:left;">
+				<div class="row">
+					<div class="col-sm-4">
+						<h3><b>Services</b></h3>
+						<h5>
+							<p>
+								Lorem ipsum
+								<br><br>
+								Dolor sit amet
+								<br><br>
+								Adicing elit
+								<br><br>
+								Sed do eiusmod
+								<br><br>
+								Tempor incididunt
+								<br><br>
+								Labore  et dolore
+								<br><br>
+								Magna aliqua
+							</p>
+						</h5>
+					</div>
+					<div class="col-sm-4" style="padding-left:5%;">
+						<h3><b>Company</b></h3>
+						<h5>
+							<p>
+								About Us
+								<br><br>
+								Contact Us
+								<br><br>
+								Help
+							</p>
+						</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php include('../includes/foot.html') ?>
